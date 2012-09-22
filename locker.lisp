671e749bccf1fecad2bffc95ae854dd4865e4e0a
Mozart Reina <mozart@mozartreina.com>
2012-09-23 08:56:09 +0900

    -*- mode: org -*- #+STARTUP: showeverything * CHANGES ** pretty printer for usage strings implemented ** usage strings written
    * TODO
    - [X] write pretty printer for usage strings
    - [X] write usage strings
    - [ ] write tests

--- locker.lisp
+++ locker.lisp
@@ -54,6 +54,12 @@
        collect (string-trim " "(subseq str x y))
        while y))
 
+(defun split-space (str)
+  (loop for x = 0 then (1+ y)
+     as y = (position #\Space str :start x)
+     collect (string-trim " "(subseq str x y))
+     while y))
+
 (defun remove-empty-lst (lst)
   (loop for x in lst
        when (> (length (car x)) 0)
@@ -126,19 +132,37 @@
         (arg2 (nth 2 cl-user::*posix-argv*))
         (arg-num (length cl-user::*posix-argv*)))
     (cond ((string-equal "edit" arg1) (edit-file arg2))
-          ((string-equal "show" arg1) (display (load-contents)
-                                          (concatenate 'string "* " (take-args))
+          ((string-equal "show" arg1) (display (load-contents) (concatenate 'string "* " (take-args))
                                         (force-output (show (cadr (hsearch query contents))))))
-          ((string-equal "find" arg1) (display (load-contents)
-                                          (take-args)
+          ((string-equal "find" arg1) (display (load-contents) (take-args)
                                         (force-output (show (isearch query (item-list contents))))))
-          ((string-equal "show-all" arg1) (display (load-contents)
-                                              nil
-                                            (format "~a" contents)))
+          ((string-equal "show-all" arg1) (display (load-contents) nil
+                                            (format t "~%~{~{~a~%~{~a~%~}~}~%~}~%" contents)))
           ((string-equal "encrypt" arg1) (write-file (open-file arg2 nil) arg2 t))
-          (t (format t "USAGE ~% edit <filename> -- unencrypts the file, passes the contents to an emacs instance. Once editing is done please remember to save the file under the same name, otherwise the changes will be saved in plaintext. File will be encrypted after exiting.")))))
+          (t (format t "~%USAGE~%~%~{~{~<~%~1,80:;~A~> ~}~%~%~}" (mapcar #'split-space (list (edit-usage-string)
+                                                                                  (show-usage-string)
+                                                                                  (find-usage-string)
+                                                                                  (show-all-usage-string)
+                                                                                  (encrypt-usage-string))))))))
+
+(defun edit-usage-string ()
+  "edit <filename> -- Unencrypts the file and passes the contents to an emacs instance. Once editing is done save the file under the same name otherwise the file will be saved unencrypted. A new passphrase for encryption will be asked upon exiting (this happens at the shell, not in the emacs instance).")
+
+(defun show-usage-string ()
+  "show <tag> <filename> -- Unencrypts the file and displays all entries under the tag. Tags are denoted by the '*' followed by a space and the tag name.")
+
+(defun find-usage-string ()
+  "find <word/phrase> <filename> -- Unencrypts the file and displays all entries matching the entered word or phrase.")
+
+(defun show-all-usage-string ()
+  "show-all <filename> -- Unencrypts the file and displays all tags and entries.")
+
+(defun encrypt-usage-string ()
+  "encrypt <filename> -- Encrypts the file. Be sure to use this only on a plaintext file, if used on an already encrypted file it will encrypt it again making it very hard to return it to a plaintext state.")
 
 
 ;;;; Testing purposes
+(setf cl-user::*posix-argv* '("locker" "show-all" "~/dev/lisp/locker/test.txt"))
+
 (defun dummy-data ()
   (defparameter *test* (load-contents)))
